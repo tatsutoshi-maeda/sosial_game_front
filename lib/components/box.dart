b88e5c77d2b34ac6907980a9653da31cd8500282
tatsutoshi-maeda <nikko8maeda@gmail.com>
2021-03-31 20:55:04 +0900

    add api button

--- lib/components/box.dart
+++ lib/components/box.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 class Box extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Box画面（TBD）'),
+      child: ElevatedButton(
+        onPressed: () {
+          getBook();
+        },
+        child: Text('API'),
+      ),
     );
+  }
+}
+
+Future<String> getBook() async {
+  try {
+    final response =
+        await http.get(new Uri.http("localhost:8080", "/v1/book/list"));
+    if (response.statusCode == 200) {
+// 成功
+      return response.body;
+    } else {
+// 失敗
+    }
+  } catch (e) {
+    return e;
   }
 }
